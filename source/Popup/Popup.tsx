f32bf81b45341f44f156cde2dbc7033f5d1fdae7
Yuku Kotani <yukukotani@gmail.com>
2022-03-10 23:12:43 +0900

    style fix

--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -1,53 +1,23 @@
-import * as React from 'react';
-import {browser, Tabs} from 'webextension-polyfill-ts';
+import * as React from "react";
+import { browser, Tabs } from "webextension-polyfill-ts";
 
-import './styles.scss';
+import "./styles.scss";
 
 function openWebPage(url: string): Promise<Tabs.Tab> {
-  return browser.tabs.create({url});
+  return browser.tabs.create({ url });
 }
 
 const Popup: React.FC = () => {
   return (
     <section id="popup">
-      <h2>WEB-EXTENSION-STARTER</h2>
+      <h2>gather-notification-extension</h2>
       <button
-        id="options__button"
-        type="button"
-        onClick={(): Promise<Tabs.Tab> => {
-          return openWebPage('options.html');
-        }}
+        onClick={() =>
+          openWebPage("https://github.com/Monchi/gather-notification-extension")
+        }
       >
-        Options Page
+        GitHub
       </button>
-      <div className="links__holder">
-        <ul>
-          <li>
-            <button
-              type="button"
-              onClick={(): Promise<Tabs.Tab> => {
-                return openWebPage(
-                  'https://github.com/abhijithvijayan/web-extension-starter'
-                );
-              }}
-            >
-              GitHub
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              onClick={(): Promise<Tabs.Tab> => {
-                return openWebPage(
-                  'https://www.buymeacoffee.com/abhijithvijayan'
-                );
-              }}
-            >
-              Buy Me A Coffee
-            </button>
-          </li>
-        </ul>
-      </div>
     </section>
   );
 };
